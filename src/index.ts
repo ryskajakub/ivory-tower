953ef2855a6dad82f9a4905ff774bcfff963a64d
Jakub Ryška <ryskajakub@seznam.cz>
2025-03-29 14:09:32 +0100

    main

--- src/index.ts
+++ src/index.ts
@@ -19,29 +19,61 @@ const tables = {
 
 type AggState = "Pre" | "Post" | "No";
 
-// class AliasExp<Path, Alias, Type, State extends AggState> { }
+type Operator = "=" | ">=" | ">";
 
-interface Exp<Type extends PgType> {
-  pgType: PgType;
+type mkResult<O extends Operator, T extends AggState> = FunExp<"boolean", T> &
+  Boolean;
+
+interface Operand<pgType extends PgType> {
+  <O extends Operator, T extends AggState>(
+    operator: O,
+    that: Exp<pgType, T>
+  ): mkResult<O, T>;
 }
 
-type AnyExp = Exp<any>;
+type mkOperand<T extends PgType> = "boolean" extends T ? Boolean : unknown;
 
-class PathExp<Path extends string, Type extends PgType, State extends AggState>
-  implements Exp<Type>
-{
-  pgType: Type;
-  type: "path_extp";
+interface Boolean {
+  AND: <aggState extends AggState>(
+    that: Exp<"boolean", AggState>
+  ) => FunExp<"boolean", aggState>;
+  OR: <aggState extends AggState>(
+    that: Exp<"boolean", AggState>
+  ) => FunExp<"boolean", aggState>;
 }
 
-type AnyPathExp = PathExp<any, any, any>;
-
-class AliasExp<Alias, Type extends PgType, State extends "Post" | "No">
-  implements Exp<Type>
-{
+abstract class Exp<Type extends PgType, State extends AggState> {
   pgType: Type;
+  aggState: State;
+
+  constructor(pgType: Type, aggState: State) {
+    this.pgType = pgType;
+    this.aggState = aggState;
+  }
+}
+
+type AnyExp = Exp<any, any>;
+
+class PathExp<
+  Table extends string,
+  Column extends string,
+  Type extends PgType,
+  State extends AggState
+> extends Exp<Type, State> {
+  type: "path_exp";
+  table: Table;
+  column: Column;
+}
+
+type AnyPathExp = PathExp<any, any, any, any>;
+
+class AliasExp<
+  Alias extends string,
+  Type extends PgType,
+  State extends "Post" | "No"
+> extends Exp<Type, State> {
   type: "alias_exp";
-  state: State;
+  alias: Alias;
 }
 
 type AnyAliasExp = AliasExp<any, any, any>;
@@ -52,8 +84,10 @@ type MkAliasExp<
   State extends AggState
 > = State extends "Post" | "No" ? AliasExp<T, Type, State> : unknown;
 
-class LitExp<Type extends PgType, State extends AggState> implements Exp<Type> {
-  pgType: Type;
+class LitExp<Type extends PgType, State extends AggState> extends Exp<
+  Type,
+  State
+> {
   type: "lit";
   AS = <T extends string>(name: T): MkAliasExp<T, Type, State> => {
     // @ts-ignore
@@ -62,6 +96,20 @@ class LitExp<Type extends PgType, State extends AggState> implements Exp<Type> {
 }
 
 type AnyLitExp = LitExp<any, any>;
+
+class FunExp<Type extends PgType, State extends AggState> extends Exp<
+  Type,
+  State
+> {}
+
+type AnyFunExp = FunExp<any, any>;
+
+type AnyNonAliasExp = AnyLitExp | AnyPathExp | AnyFunExp;
+
+type NonAliasExp<Type extends PgType, State extends AggState> =
+  | LitExp<Type, State>
+  | PathExp<any, any, Type, State>
+  | FunExp<Type, State>;
 
 // @ts-ignore
 const x: Exp<"abc", "def", "No"> = null;
@@ -72,7 +120,7 @@ class JsonB<T> {
   t: T;
 }
 
-type PgType = "int" | "text" | JsonB<any> | null;
+type PgType = "boolean" | "int" | "text" | JsonB<any> | null;
 
 // type MkJsonBArrayAggType<T extends PgType> = T extends "int"
 //   ? JsonB<number[]>
@@ -96,12 +144,12 @@ type MkJsonBTypeNoNull<T extends PgType> = T extends "int"
   ? U
   : never;
 
-type JsonbAggResult<T extends PathExp<any, PgType, "Pre" | "No">> =
-  T extends PathExp<any, infer Type extends PgType, any>
-    ? LitExp<MkJsonBArrayAggType<Type>, "Post">
+type JsonbAggResult<T extends NonAliasExp<PgType, "Pre" | "No">> =
+  T extends AnyNonAliasExp
+    ? LitExp<MkJsonBArrayAggType<T["pgType"]>, "Post">
     : unknown;
 
-const jsonb_agg = <T extends PathExp<any, any, "Pre" | "No">>(
+const jsonb_agg = <T extends AnyNonAliasExp>(
   exp: T
   // ): JsonbAggResult<T>["type"] extends PgType ? LitExp<JsonbAggResult<T>["type"]> : unknown => {
 ): JsonbAggResult<T> => {
@@ -111,31 +159,53 @@ const jsonb_agg = <T extends PathExp<any, any, "Pre" | "No">>(
 
 // type XXXX = JsonbAggResult<PathExp<any, "text", "No">>
 
-type MkJsonBuildObject<T extends any[], Result> = T["length"] extends 0
-  ? JsonB<Result>
-  : T extends [infer Name extends string, infer E, ...infer Rest]
-  ? E extends Exp<any>
-    ? MkJsonBuildObject<Rest, Result & { [K in Name]: MkJsonBType<E["pgType"]> }>
-    : never
+// type T<X> = X
+
+type mkState<state extends AggState> = "Pre" extends state
+  ? "Pre"
+  : "Post" extends state
+  ? "Post"
+  : state;
+
+type MkJsonBuildObjectGo<T extends any[], Result, StateAcc extends AggState> = [
+  T["length"],
+  StateAcc
+] extends [0, infer stateAcc extends AggState]
+  ? FunExp<JsonB<ExpandRecursively<Result>>, mkState<stateAcc>>
+  : T extends [
+      infer Name extends string,
+      infer E extends AnyNonAliasExp,
+      ...infer Rest
+    ]
+  ? "Pre" | "Post" extends E["aggState"] | StateAcc
+    ? unknown
+    : MkJsonBuildObjectGo<
+        Rest,
+        Result & { [K in Name]: MkJsonBType<E["pgType"]> },
+        E["aggState"] | StateAcc
+      >
   : never;
 
-type DEF = MkJsonBType<"text">;
+type MkJsonBuildObject<T extends any[]> = MkJsonBuildObjectGo<T, {}, never>;
 
 type ABC = MkJsonBuildObject<
   [
-    "name",
-    PathExp<any, "text", any>,
+    // "name",
+    // PathExp<any, "text", "Post">,
     "id",
-    PathExp<any, "int", any>,
-    "js",
-    PathExp<any, JsonB<number[]>, any>
-  ],
-  {}
+    PathExp<any, any, "int", "Pre">
+    // "js",
+    // FunExp<"text", "No">
+  ]
 >;
 
+// type MMM = "Pre" | "Post" extends "Post" ? true : false;
+
+// type MkFunExp<pgType extends PgType, >
+
 const jsonb_build_object = <const T extends any[]>(
-  t: T
-): MkJsonBuildObject<T, {}> => {
+  ...t: T
+): MkJsonBuildObject<T> => {
   // @ts-ignore
   return null;
 };
@@ -143,9 +213,9 @@ const jsonb_build_object = <const T extends any[]>(
 type Lit = number | string;
 
 type MkLit<T extends Lit> = T extends number
-  ? PathExp<any, "int", "No">
+  ? PathExp<any, any, "int", "No">
   : T extends string
-  ? PathExp<any, "text", "No">
+  ? PathExp<any, any, "text", "No">
   : never;
 
 const mkLit = <T extends Lit>(t: T): MkLit<T> => {
@@ -206,24 +276,46 @@ interface TableColumn {
 //     [K in keyof ]
 // }
 
-type MkColumns<Table extends readonly any[]> = Table["length"] extends 0
+type QueriedTableType = readonly (readonly [string, AnyPathExp])[];
+type QueriedTablesType = Record<string, QueriedTableType>;
+
+type MkColumns<
+  Table extends readonly (readonly [string, any])[],
+  Agg extends boolean,
+  AggColumns extends string
+> = Table["length"] extends 0
   ? {}
   : Table extends readonly [
-      readonly [infer ColName extends string, infer ColType],
-      ...infer Rest
+      readonly [infer ColName extends string, infer ColType extends AnyPathExp],
+      ...infer Rest extends readonly (readonly [string, any])[]
     ]
   ? {
-      [K in ColName]: ColType;
-    } & MkColumns<Rest>
+      [K in ColName]: Agg extends true
+        ? PathExp<
+            ColType["table"],
+            ColType["column"],
+            ColType["pgType"],
+            ColName extends AggColumns ? "Post" : "Pre"
+          >
+        : ColType & Operand<ColType["pgType"]> & mkOperand<ColType["pgType"]>;
+    } & MkColumns<Rest, Agg, AggColumns>
   : never;
 
 // type XXX = MkColumns<[["abc", { type: "text" }], ["tef", { type: "int" }]]>;
+type XXX2 = MkColumns<
+  [["col1", PathExp<"abc", "col1", "int", "No">], ["col2", PathExp<"abc", "col2", "text", "No">]],
+  true,
+  "col1"
+>;
+
+type T = [][number];
 
 type C = readonly [any, any];
 
 type MkTables<
   Tables extends Record<string, any>,
-  Froms extends readonly C[]
+  Froms extends readonly C[],
+  Group extends Record<string, readonly string[]>
 > = Froms["length"] extends 0
   ? {}
   : Froms extends readonly [
@@ -231,14 +323,15 @@ type MkTables<
       ...infer Rest extends readonly (readonly [keyof Tables, string])[]
     ]
   ? {
-      [K in Alias]: MkColumns<Tables[Name]>; //MkColumns<Tables[Name]>;
-    } & MkTables<Tables, Rest>
+      [K in Alias]: MkColumns<
+        Tables[Name],
+        {} extends Group ? false : true,
+        Name extends keyof Group ? Group[Name][number] : never
+      >; //MkColumns<Tables[Name]>;
+    } & MkTables<Tables, Rest, Group>
   : never;
 
-type TTT = MkTables<
-  typeof tables,
-  readonly [readonly ["person", "person"], readonly ["pet", "p"]]
->;
+type X = {} extends {} ? 1 : 2;
 
 // @ts-ignore
 // const x: TTT = null;
@@ -261,7 +354,7 @@ type MkColumnExp<
   TableName extends string,
   ColumnName extends string,
   ColumnData extends TableColumn
-> = PathExp<ColumnName, MkType<ColumnData>, "No">;
+> = PathExp<TableName, ColumnName, MkType<ColumnData>, "No">;
 
 type MkQueryColumns<
   TableName extends string,
@@ -287,19 +380,18 @@ type MkQueryTables<Tables extends Record<string, any>> = {
 
 type TTT2 = MkQueryTables<typeof tables>;
 
-class SelectAs<Exps extends readonly AnySelectable[], Name extends string> {
-  type: "select_as";
-}
+// class SelectAs<Exps extends readonly AnySelectable[], Name extends string> {
+//   type: "select_as";
+// }
 
-class SelectAsExp<AnyS extends AnySelectable, Name extends string>
-  extends SelectAs<AnyS[], Name>
-  implements Exp<AnyS["pgType"]>
-{
-  pgType: AnyS["pgType"];
-  type2: "select_as";
-}
+// class SelectAsExp<AnyS extends AnySelectable, Name extends string>
+//   extends SelectAs<AnyS[], Name>
+//    Exp<AnyS["pgType"], "No">
+// {
+//   type2: "select_as";
+// }
 
-type AnySelectAsExp = SelectAsExp<any, any>;
+// type AnySelectAsExp = SelectAsExp<any, any>;
 
 type IsSingle<Exps extends readonly any[]> = Exps["length"] extends 1
   ? never
@@ -311,22 +403,35 @@ type AllPostAgg<Exps extends readonly AnySelectable[]> = Exps extends readonly [
   ? never
   : unknown;
 
-type MkSelectAs<T extends string, Exps extends readonly AnySelectable[]> =
-  // [unknown, Exps] extends [IsSingle<Exps> | AllPostAgg<Exps>, infer Exp extends [infer OnlyOne extends AnySelectable]] ?
-  [Exps["length"], Exps] extends [
-    1,
-    infer A extends [AliasExp<infer Name, infer Type, "Post">]
-  ]
-    ? SelectAsExp<AliasExp<Name, Type, "Post">, T>
-    : unknown;
+// type MkSelectAs<T extends string, Exps extends readonly AnySelectable[]> =
+//   // [unknown, Exps] extends [IsSingle<Exps> | AllPostAgg<Exps>, infer Exp extends [infer OnlyOne extends AnySelectable]] ?
+//   [Exps["length"], Exps] extends [
+//     1,
+//     infer A extends [AliasExp<infer Name, infer Type, "Post">]
+//   ]
+//     ? SelectAsExp<AliasExp<Name, Type, "Post">, T>
+//     : unknown;
 
 // type abcdef = AllPostAgg<readonly [AliasExp<any, any, "Post">]>;
 
+class SelectSingleColumn<
+  Exp extends AnySelectable,
+  pgType extends PgType
+> extends Exp<pgType, "No"> {
+  type: "select_single_column";
+}
+
+class SelectSingleColumnAndRow<
+  Exp extends AnySelectable,
+  pgType extends PgType
+> extends Exp<pgType, "No"> {}
+
 class Select<Exps extends readonly AnySelectable[]> {
-  AS = <T extends string>(alias: T): MkSelectAs<T, Exps> => {
-    // @ts-ignore
-    return null;
-  };
+  type: "select";
+  // AS = <T extends string>(alias: T): MkSelectAs<T, Exps> => {
+  //   // @ts-ignore
+  //   return null;
+  // };
 }
 
 // type T = true extends false ? 1 : 2
@@ -339,12 +444,84 @@ class Select<Exps extends readonly AnySelectable[]> {
 
 // type XXX1 = readonly [SelectAs<any, any, false>] extends readonly (AnySingleAsSelect | AnyLitExp )[] ? 1 : 2
 
-type MkSelect<Exps extends readonly AnySelectable[]> = Select<Exps>;
+type WhereExp = LitExp<"boolean", "No"> | FunExp<"boolean", "No">;
+
+type mkGroupByGo<
+  Q extends readonly AnyGroupExp[],
+  Acc extends Record<string, string[]>
+> = Q["length"] extends 0
+  ? Acc
+  : Q extends readonly [
+      infer Head,
+      ...infer Rest extends readonly AnyGroupExp[]
+    ]
+  ? Head extends PathExp<any, any, any, "No">
+    ? mkGroupByGo<
+        Rest,
+        {
+          [K in Head["table"]]: [
+            ...(unknown extends Acc[K] ? [] : Acc[K]),
+            Head["column"]
+          ];
+        } & Omit<Acc, Head["table"]>
+      >
+    : mkGroupByGo<Rest, Acc>
+  : never;
+
+type mkGroupBy<Q extends readonly AnyGroupExp[]> = ExpandRecursively<
+  mkGroupByGo<Q, {}>
+>;
+
+// type mkX <rec extends Record<string, number>, str extends string> = rec[str]
+
+// type TTT8 = mkX<{ "abc": 3 }, "def">
+
+// type TTTTT = mkGroupBy<readonly [PathExp<"abc", "def", "boolean", "No">, PathExp<"xxx", "lol", "int", "No">, FunExp<"text", "No">, PathExp<"xxx", "wtf", "int", "No">]>;
+
+type MkSelect<Exps extends readonly AnySelectable[]> = [
+  Exps["length"],
+  Exps
+] extends [1, [infer E extends AnySelectable]]
+  ? SelectSingleColumn<E, AnyExp["pgType"]>
+  : Select<Exps>;
 
 const mkDb = <Tables extends TablesType>(tables: Tables) => {
   type QueryTables = MkQueryTables<Tables>;
 
-  class Join<Selected extends readonly (readonly [any, any])[]> {
+  class Data<
+    Selected extends readonly (readonly [any, any])[],
+    Grouped extends Record<string, readonly string[]>
+  > {
+    selected: Selected;
+    grouped: Grouped;
+  }
+
+  class GroupBy<Selected extends readonly (readonly [any, any])[]> extends Data<
+    Selected,
+    {}
+  > {
+    GROUP_BY = <const Q extends readonly AnyGroupExp[]>(
+      fun: (tables: MkTables<QueryTables, Selected, {}>) => Q
+    ): Data<Selected, mkGroupBy<Q>> => {
+      // @ts-ignore
+      return null;
+    };
+  }
+
+  class Where<
+    Selected extends readonly (readonly [any, any])[]
+  > extends GroupBy<Selected> {
+    WHERE = (
+      fun: (tables: MkTables<QueryTables, Selected, {}>) => WhereExp
+    ): GroupBy<Selected> => {
+      // @ts-ignore
+      return null;
+    };
+  }
+
+  class Join<
+    Selected extends readonly (readonly [any, any])[]
+  > extends Where<Selected> {
     public JOIN<
       T extends keyof Tables,
       Options extends { AS: string },
@@ -366,11 +543,12 @@ const mkDb = <Tables extends TablesType>(tables: Tables) => {
 
   const SELECT = <
     Froms extends readonly C[],
+    Group extends Record<string, string[]>,
     const Q extends readonly AnySelectable[]
   >(
     // fun: <Q>(tables: Froms) => Q,
-    fun: (tables: MkTables<QueryTables, Froms>) => Q,
-    j: Join<Froms>
+    fun: (tables: MkTables<QueryTables, Froms, Group>) => Q,
+    j: Data<Froms, Group>
   ): MkSelect<Q> => {
     // @ts-ignore
     return null;
@@ -406,7 +584,9 @@ const from = FROM("person")
 //   "abc"
 // );
 
-type AnySelectable = AnyPathExp | AnyAliasExp | AnySelectAsExp;
+type AnySelectable = AnyPathExp | AnyAliasExp;
+
+type AnyGroupExp = AnyPathExp | AnyLitExp | AnyFunExp;
 
 type NonNullableResult<Type extends PgType> = Type extends "text"
   ? string
@@ -426,13 +606,13 @@ type SelectResult<Exps extends readonly AnySelectable[]> =
   Exps["length"] extends 0
     ? []
     : Exps extends readonly [
-        infer Head extends Exp<any>,
+        infer Head extends AnyExp,
         ...infer Rest extends readonly AnySelectable[]
       ]
     ? [ColResult<Head["pgType"]>, ...SelectResult<Rest>]
     : never;
 
-type SR = SelectResult<[AliasExp<"N", "int", "Post">]>
+type SR = SelectResult<[AliasExp<"N", "int", "Post">]>;
 
 type ExpandedSelectResult<S extends Select<any>> = S extends Select<
   infer E extends readonly AnySelectable[]
@@ -448,11 +628,22 @@ type ExpandedSelectResult<S extends Select<any>> = S extends Select<
 //   FROM("person").JOIN("pet", { AS: "p" })
 // );
 const f = SELECT((t) => {
-  const x = jsonb_agg(t.person.id).AS("person_ids");
+  const tt = jsonb_build_object("person", t.person.id, "n", t.person.name);
+
+  const x = jsonb_agg(tt).AS("person_ids");
   //   type T = typeof x;
   //   type MMM = typeof x extends Exp<infer Type> ? ColResult<Type> : never;
   return [x];
 }, FROM("person"));
+
+// const t1231 =
+
+// SELECT( t => ,FROM("person")
+//   .WHERE((x) =>
+//     x.person.id("=", x.person.id).AND(x.person.id("=", x.person.id))
+//   )
+//   .GROUP_BY((x) => [x.person.name])
+// )
 
 type AUAU = ExpandedSelectResult<typeof f>;
 
